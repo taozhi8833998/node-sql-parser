be0f1c3825171a71b3bbc4f7174a1356f1b7ef8f
taozhi8833998 <taozhi8833998@163.com>
2020-01-10 15:39:07 +0800

    support select over partition

--- src/aggregation.js
+++ src/aggregation.js
@@ -1,17 +1,19 @@
 import has from 'has'
 import { exprToSQL } from './expr'
+import { hasVal, identifierToSql } from './util'
 
 function aggrToSQL(expr) {
   /** @type {Object} */
-  const { args } = expr
+  const { args, over } = expr
   let str = exprToSQL(args.expr)
   const fnName = expr.name
+  let overStr = ''
 
   if (fnName === 'COUNT') {
     if (has(args, 'distinct') && args.distinct !== null) str = `DISTINCT ${str}`
+    if (over) overStr = `OVER (PARTITION BY ${over.map(col => identifierToSql(col)).join(', ')})`
   }
-
-  return `${fnName}(${str})`
+  return [`${fnName}(${str})`, overStr].filter(hasVal).join(' ')
 }
 
 export {
