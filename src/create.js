906a108452b09c2bddb5f4cb346cd02dec304309
taozhi8833998 <taozhi8833998@163.com>
2020-04-26 10:26:59 +0800

    support uuid_generate_v4() and create trigger in postgresql

--- src/create.js
+++ src/create.js
@@ -1,10 +1,12 @@
 
+import { exprToSQL } from './expr'
 import { indexDefinitionToSQL } from './index-definition'
 import { columnDefinitionToSQL } from './column'
 import { constraintDefinitionToSQL } from './constrain'
-import { tablesToSQL, tableOptionToSQL } from './tables'
+import { funcToSQL } from './func'
+import { tablesToSQL, tableOptionToSQL, tableToSQL } from './tables'
 import { unionToSQL } from './union'
-import { toUpper, hasVal } from './util'
+import { toUpper, hasVal, identifierToSql, columnRefToSQL } from './util'
 
 function createDefinitionToSQL(definition) {
   if (!definition) return []
@@ -21,7 +23,7 @@ function createDefinitionToSQL(definition) {
   }
 }
 
-function createToSQL(stmt) {
+function createTableToSQL(stmt) {
   const {
     type, keyword, table, like, as, temporary,
     if_not_exists: ifNotExists,
@@ -46,6 +48,52 @@ function createToSQL(stmt) {
   sql.push(toUpper(ignoreReplace), toUpper(as))
   if (queryExpr) sql.push(unionToSQL(queryExpr))
   return sql.filter(hasVal).join(' ')
+}
+
+function createTriggerToSQL(stmt) {
+  const {
+    constraint,
+    constraint_kw: constraintKw,
+    deferrable,
+    events,
+    execute,
+    for_each: forEach,
+    from,
+    keyword,
+    type,
+    location,
+    table,
+    when,
+  } = stmt
+  const sql = [toUpper(type), toUpper(constraintKw), toUpper(keyword), identifierToSql(constraint), toUpper(location)]
+  const eventList = events.map(event => {
+    const { keyword: kw, args } = event
+    const result = [toUpper(kw)]
+    if (args) {
+      const { keyword: kwArgs, columns } = args
+      result.push(toUpper(kwArgs), columns.map(columnRefToSQL).join(', '))
+    }
+    return result.join(' ')
+  }).join(' OR ')
+  sql.push(eventList, 'ON', tableToSQL(table))
+  if (from) sql.push('FROM', tableToSQL(from))
+  if (deferrable) sql.push(toUpper(deferrable.keyword), toUpper(deferrable.args))
+  if (forEach) sql.push(toUpper(forEach.keyword), toUpper(forEach.args))
+  if (when) sql.push(toUpper(when.type), exprToSQL(when.cond))
+  sql.push(toUpper(execute.keyword), funcToSQL(execute.expr))
+  return sql.filter(hasVal).join(' ')
+}
+
+function createToSQL(stmt) {
+  const { keyword } = stmt
+  switch (keyword.toLowerCase()) {
+    case 'table':
+      return createTableToSQL(stmt)
+    case 'trigger':
+      return createTriggerToSQL(stmt)
+    default:
+      throw new Error(`unknow create resource ${keyword}`)
+  }
 }
 
 export {
