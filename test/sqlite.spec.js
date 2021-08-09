42ba301ee409a88f7bf284d91c322ed83daf6b68
taozhi8833998 <taozhi8833998@163.com>
2021-08-09 11:38:34 +0800

    refactor: upgrade version

--- test/sqlite.spec.js
+++ test/sqlite.spec.js
@@ -2,20 +2,20 @@ const { expect } = require('chai');
 const Parser = require('../src/parser').default
 
 describe('sqlite', () => {
-	const parser = new Parser();
+  const parser = new Parser();
 
-	function getParsedSql(sql, opt = { database: 'sqlite' }) {
-		const ast = parser.astify(sql, opt);
-		return parser.sqlify(ast, opt);
+  function getParsedSql(sql, opt = { database: 'sqlite' }) {
+    const ast = parser.astify(sql, opt);
+    return parser.sqlify(ast, opt);
   }
 
-	it('should support analyze', () => {
-		const sql = 'analyze schemaName.tableName'
-		expect(getParsedSql(sql)).to.be.equal('ANALYZE `schemaName`.`tableName`')
-	})
+  it('should support analyze', () => {
+    const sql = 'analyze schemaName.tableName'
+    expect(getParsedSql(sql)).to.be.equal('ANALYZE `schemaName`.`tableName`')
+  })
 
-	it('should support attach', () => {
-		const sql = "attach database 'c:\sqlite\db\contacts.db' as contacts;"
-		expect(getParsedSql(sql)).to.be.equal("ATTACH DATABASE 'c:\sqlite\db\contacts.db' AS `contacts`")
-	})
+  it('should support attach', () => {
+    const sql = "attach database 'c:\sqlite\db\contacts.db' as contacts;"
+    expect(getParsedSql(sql)).to.be.equal("ATTACH DATABASE 'c:\sqlite\db\contacts.db' AS `contacts`")
+  })
 })
