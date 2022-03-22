b37512228977a60bde49c8c0ddf63c4a23929779
taozhi8833998 <taozhi8833998@163.com>
2022-03-22 14:14:20 +0800

    refactor: add test cases

--- test/mysql-mariadb.spec.js
+++ test/mysql-mariadb.spec.js
@@ -251,6 +251,13 @@ describe('mysql', () => {
           "SELECT * FROM `tableName` WHERE POSITION('\n' IN `largeText`) > 0"
         ],
       },
+      {
+        title: 'in bracket in column',
+        sql: [
+          "SELECT POSITION('\n' IN `largeText`) AS `charPosition` FROM `tableName`;",
+          "SELECT POSITION('\n' IN `largeText`) AS `charPosition` FROM `tableName`"
+        ],
+      },
     ]
     SQL_LIST.forEach(sqlInfo => {
       const { title, sql } = sqlInfo
